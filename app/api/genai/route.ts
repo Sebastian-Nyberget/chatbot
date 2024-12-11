2fe67bce5e82d2aa9d92575475321e6e4cdc9222
Sebastian-Nyberget <nybesebe@gmail.com>
2024-12-11 08:52:51 +0100

    Added data instead of systemInstruction

--- app/api/genai/route.ts
+++ app/api/genai/route.ts
@@ -1,21 +1,17 @@
 import { StreamingTextResponse, GoogleGenerativeAIStream } from "ai";
-import { GoogleGenerativeAI, ModelParams } from "@google/generative-ai";
-
-interface CustomModelParams extends ModelParams {
-    systemInstruction?: string;
-  }
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export async function POST(req: Request) {
     const reqBody = await req.json();
     console.log(reqBody);
     const prompt = reqBody.data.prompt;
+    const data = "You are an AI assistant specializing in fitness and nutrition, providing friendly, motivational, and scientifically-backed advice on workout routines, diet plans, and wellness, tailoring recommendations to user goals while emphasizing balance, consistency, and sustainability, avoiding medical advice, and ensuring users feel supported and empowered."
     const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!);
     const model = genAI.getGenerativeModel({ 
         model: "gemini-pro",
-        systemInstruction: "You are an AI assistant specializing in fitness and nutrition, providing friendly, motivational, and scientifically-backed advice on workout routines, diet plans, and wellness, tailoring recommendations to user goals while emphasizing balance, consistency, and sustainability, avoiding medical advice, and ensuring users feel supported and empowered."
-    } as CustomModelParams);
+     });
     
 
- const streamingResponse = await model.generateContentStream(prompt);
+ const streamingResponse = await model.generateContentStream(data + prompt);
  return new StreamingTextResponse(GoogleGenerativeAIStream(streamingResponse));
 }
